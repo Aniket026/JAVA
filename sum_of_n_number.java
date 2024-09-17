af8acd842c17d7f8875367a2f14308b123924835
Aniket Borawake <131742395+Aniket026@users.noreply.github.com>
2024-09-17 08:57:50 +0530

    loops while, for

---
+++ sum_of_n_number.java
@@ -0,0 +1,67 @@
+import java.util.*;
+/*public class loops{
+    public static void main(String[] args){
+        for (int i=0;i<3;i++){
+            System.out.println("Aniket");
+            System.out.println(i);
+        }
+    }
+}
+   
+
+public class loops{
+    public static void main(String[] args){
+        int i=0;
+        while(i<=20){
+            System.out.println("Aniket");
+            System.out.println(i);
+            i=i+1;
+        }
+    }
+}
+
+ 
+ public class loops{
+    public static void main(String[] args){
+        int i=0;
+        do{
+          System.out.println("Aniket");
+          System.out.println(i);
+          i=i+1;
+
+        }while(i<=10);
+    }
+}
+
+
+
+public class sum_of_n_number {
+       public static void main(String[] args){
+         Scanner sc =new Scanner (System.in);
+    int number = sc.nextInt();
+    int sum=0;
+    int i=1;
+    while(i <= number){
+        sum=sum+i;
+        i=i+1;
+        System.out.println(sum);
+    }
+    
+   }
+}
+
+
+public class sum_of_n_number {
+    public static void main(String[] args){
+      Scanner sc =new Scanner (System.in);
+ int number = sc.nextInt();
+ int sum=0;
+ 
+for (int i=1;i<11;i++){
+    System.out.println(number*i);
+}
+ 
+}
+}
+
+*/

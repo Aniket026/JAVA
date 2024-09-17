f5ec77b49b2a15684df9fe7748fa2450d7c4e134
Aniket Borawake <131742395+Aniket026@users.noreply.github.com>
2024-09-17 08:57:10 +0530

    input and output

---
+++ input.java
@@ -0,0 +1,39 @@
+/*public class variables {
+    public static void main(String[] args) {
+        String name = "aniket";
+        int a=4;
+        int b=9;
+        int sum = a+b;
+        int sub = a-b;
+        int mul = a*b;
+        System.out.println(name); 
+        System.out.println(a+b); 
+        System.out.println(sum); 
+        System.out.println(sub);
+        System.out.println(mul);
+    }
+}
+*/
+/*
+
+public class claculation {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+        int sum=a*b;
+        int sum2=a-b;
+        System.out.println(sum/sum2);
+
+    }
+}*/
+
+import java.util.*;
+public class input {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b ;
+        System.out.println(sum);
+    }
+}

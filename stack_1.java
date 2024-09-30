38a59ecd76680c9abdf32bc0d9736903d6784875
Aniket Borawake <131742395+Aniket026@users.noreply.github.com>
2024-09-30 22:20:35 +0530

    Add files via upload

---
+++ stack_1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class stack_1 {
+
+public static class stack{
+    Stack<Integer> stack1 =new stack<>();
+
+
+}
+
+
+    public static void main(String[] args)
+    {
+
+        stack stack1 =new stack()
+         stack1.push(1);
+        stack1.push(2);
+        stack1.push(3);
+        stack1.push(4);
+    
+
+    }
+
+    
+}
